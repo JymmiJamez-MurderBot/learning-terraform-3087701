a8e7e813fe4f02736ca3613df7b58ea63ed7c5a6
JymmiJamez-MurderBot <James.W.Welch3@gmail.com>
2025-06-12 16:36:55 -0400

    back to line 91

--- main.tf
+++ main.tf
@@ -88,7 +88,7 @@ resource "aws_lb_listener" "blog_listener" {
 # Associate a target group with the ALB's listener
 resource "aws_lb_target_group_attachment" "blog_tg_attach" {
   target_group_arn      = aws_lb_target_group.blog_tg.arn
-  target_id             = module.blog-asg.asg
+  target_id             = module.autoscaling.asg
   port                  = aws_lb_listener.blog_listener.port
 }
 
