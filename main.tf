48381103d2f475bcac8c61c9e50c48794bf677c0
JymmiJamez-MurderBot <James.W.Welch3@gmail.com>
2025-06-12 12:25:46 -0400

    fix line 54

--- main.tf
+++ main.tf
@@ -48,10 +48,10 @@ resource "aws_instance" "blog" {
 module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
-  name            = "blog-alb"
-  vpc_id          = [module.blog_vpc.vpc_id]
-  subnets         = [module.blog_vpc.public_subnets]
-  security_group  = [module.blog_security-group.security_group_id]
+  name             = "blog-alb"
+  vpc_id           = [module.blog_vpc.vpc_id]
+  subnets          = [module.blog_vpc.public_subnets]
+  security_groups  = [module.blog_security-group.security_group_id]
 
 target_groups = {
   ex-instance = {
