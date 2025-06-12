857766f267f14c5f5d9a62dfd65a8eda905e1b58
JymmiJamez-MurderBot <James.W.Welch3@gmail.com>
2025-06-12 15:26:33 -0400

    T5

--- main.tf
+++ main.tf
@@ -88,7 +88,7 @@ resource "aws_lb_listener" "blog_listener" {
 # Associate a target group with the ALB's listener
 resource "aws_alb_target_group_attachment" "blog_tg_attach" {
   target_group_arn      = aws_lb_target_group.blog_tg.arn
-  target_id             = aws_lb_listener.blog_alb.load_balancer_arn
+  target_id             = aws_lb_listener.blog_alb.arn
   port                  = aws_lb_listener.blog_listener.port
 }
 
