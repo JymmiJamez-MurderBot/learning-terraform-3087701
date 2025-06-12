1d8c580fc157599f835475ec752b57d9af02f72f
JymmiJamez-MurderBot <James.W.Welch3@gmail.com>
2025-06-12 12:32:15 -0400

    updating formatting line 54

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ module "alb" {
   name             = "blog-alb"
   vpc_id           = module.blog_vpc.vpc_id
   subnets          = module.blog_vpc.public_subnets
-  security_groups  = module.blog_security-group.security_group_id
+  security_groups  = [module.blog_security-group.security_group_id]
 
 target_groups = {
   ex-instance = {
@@ -75,14 +75,14 @@ tags = {
  }
 
 module "blog_security-group" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.3.0"
-  name = "blog_new" 
+  source              = "terraform-aws-modules/security-group/aws"
+  version             = "5.3.0"
+  name                = "blog_new" 
 
-  vpc_id = module.blog_vpc.vpc_id
-  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  vpc_id              = module.blog_vpc.vpc_id
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
