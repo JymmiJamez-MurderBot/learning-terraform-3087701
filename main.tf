a10ac2510da55ed675c32ddfa15cb816e6a23ad2
JymmiJamez-MurderBot <James.W.Welch3@gmail.com>
2025-06-12 16:58:58 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,90 +18,24 @@ data "aws_vpc" "default" {
   default = true
 }
 
-module "blog_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+resource "aws_instance" "blog" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
-    Terraform = "true"
-    Environment = "dev"
+    Name = "Learning Terraform"
   }
 }
 
-module "autoscaling" {
-  source        = "terraform-aws-modules/autoscaling/aws"
-  version       = "8.3.0"
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
 
-  name          = "blog-asg"
-  min_size      = 1
-  max_size      = 2
-  
-  vpc_zone_identifier     = module.blog_vpc.public_subnets
-  security_groups         = [module.blog_security-group.security_group_id]
-
-  image_id                = data.aws_ami.app_ami.id
-  instance_type           = var.instance_type
-  
-  tags = {
-    Name = "dev Learning Terraform"
-  }
-}
-
-resource "aws_lb" "blog_alb" {
-  name                  = "blog-alb"
-  load_balancer_type    = "application"
-  subnets               = module.blog_vpc.public_subnets
-  security_groups       = [module.blog_security-group.security_group_id]
-  
-  tags = {
-    Environment = "dev"
- }
-}
-
-resource "aws_lb_target_group" "blog_tg" {
-  name                = "blog-tg"
-  port                = 80
-  protocol            = "HTTP"
-  vpc_id              = module.blog_vpc.vpc_id
-  
-  tags = {
-    Environment = "dev"
-  }
- }
-
-resource "aws_lb_listener" "blog_listener" {
-  load_balancer_arn    = aws_lb.blog_alb.arn
-  port                 = "80"
-  protocol             = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.blog_tg.arn
-  }
-}
-
-# Associate a target group with the ALB's listener
-resource "aws_lb_target_group_attachment" "blog_tg_attach" {
-  target_group_arn      = aws_lb_target_group.blog_tg.arn
-  target_id             = module.autoscaling.blog-asg.asg
-  port                  = aws_lb_listener.blog_listener.port
-}
-
-
-module "blog_security-group" {
-  source              = "terraform-aws-modules/security-group/aws"
-  version             = "5.3.0"
-  name                = "blog_new" 
-
-  vpc_id              = module.blog_vpc.vpc_id
-  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  vpc_id  = data.aws_vpc.default.id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules        = ["all-all"]
-  egress_cidr_blocks  = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
