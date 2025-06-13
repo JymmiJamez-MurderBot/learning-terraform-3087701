00f772bbb0359dd69f36332691b864788bafbd9a
JymmiJamez-MurderBot <James.W.Welch3@gmail.com>
2025-06-13 13:53:19 -0400

    fix2

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "app_ami" {
   most_recent = true
-  
+
   filter {
     name   = "name"
     values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
@@ -43,13 +43,13 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "blog-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-      targets          = {
-        target_id      = aws_instance.blog.id
-        port           = 80
+      name_prefix         = "blog-"
+      backend_protocol    = "HTTP"
+      backend_port        = 80
+      target_type         = "instance"
+        targets           = {
+        target_id         = aws_instance.blog.id
+        port              = 80
     }
   ]
 
