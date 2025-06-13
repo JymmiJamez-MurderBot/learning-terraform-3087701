b8e9f598f4261b3d8b9b18d9c79d76eee41c7c4d
JymmiJamez-MurderBot <James.W.Welch3@gmail.com>
2025-06-13 14:12:18 -0400

    4_2

--- main.tf
+++ main.tf
@@ -29,6 +29,57 @@ module "blog_vpc" {
   }
 }
 
+module "alb {
+  source    = "terraform-aws-modules/alb/aws
+  version   = "~> 6.0"
+
+  name      = "blog_alb"
+
+  load_balancer_type  = "application"
+
+  vpc_id              = module.blog_vpc.vpc_id
+  public_subnets      = module.blog_vpc.public_subnets
+  security_groups     = [module.blog_sg.security_group_id]
+
+  target_groups = [
+    {
+      name_prefix       = "blog-"
+      backend_protocol  = "HTTP"
+      backend_port      = 80
+      target_type       = "instance"
+       my_target = {
+          target_id   = aws_instance.blog.id
+          port        = 80
+       }
+    }
+  ]
+
+  http_tcp_listener = [
+    {
+      port                = 80
+      protocol            = "HTTP"
+      target_group_index  = 0
+  }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "8.3.0"
+  
+  name            = "blog"
+  min_size        = 1
+  max_size        = 2
+
+  vpc_zone_indentifier  = module.blog_vpc.public_subnets
+  target_group_arns     = module.blog_alb
+}
+
+
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
