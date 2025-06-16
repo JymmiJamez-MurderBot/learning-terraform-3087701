77116c878d9d665a72ac0ef710c39a838a3c332b
JymmiJamez-MurderBot <James.W.Welch3@gmail.com>
2025-06-16 11:13:17 -0400

    --Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,12 +9,13 @@ variable "ami_filter" {
     name    = string
     owner   = string
   })
-  default =   {
+  default = {
    {
     name        = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
     owner       = "979382823631"
-    }
+   }
  }
+}
 
 variable "environment" {
   description = "Development Environment"
