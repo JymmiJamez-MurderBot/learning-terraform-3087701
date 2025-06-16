bb4898fb3a6b7d731f5d395a33385227a202ca5a
JymmiJamez-MurderBot <James.W.Welch3@gmail.com>
2025-06-16 11:18:48 -0400

    AI help Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,17 +4,16 @@ variable "instance_type" {
 }
 
 variable "ami_filter" {
-  description = "Name filter and owner for AMI"
-  type = object({
-    name    = string
-    owner   = string
-  })
-  default = {
-   {
-    name        = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
-    owner       = "979382823631"
-   }
- }
+  type = list(object({
+    name = string
+    owner = string
+  }))
+  default = [
+    {
+      name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+      owner = "979382823631"
+    }
+  ]
 }
 
 variable "environment" {
