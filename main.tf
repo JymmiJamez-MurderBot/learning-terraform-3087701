6cb83acb8f75cdd58a5cbf9ada849c52dc8e9e94
JymmiJamez-MurderBot <James.W.Welch3@gmail.com>
2025-06-16 14:22:40 -0400

    fixing

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ module "blog_vpc" {
   cidr = "10.0.100.0/22"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["${var.environment.network_prefix}.100.0/24", "(${var.environment.network_prefix}).101.0/24", "(${var.environment.network_prefix}).102.0/24"]
+  public_subnets  = ["{var.environment.network_prefix}.100.0/24", "{var.environment.network_prefix}.101.0/24", "{var.environment.network_prefix}.102.0/24"]
 
   tags = {
     Terraform = "true"
@@ -87,7 +87,7 @@ module "blog_sg" {
   version = "5.3.0"
 
   vpc_id                = module.blog_vpc.vpc_id
-  name                  = "${var.environment.name}-blog"
+  name                  = "var.environment.name}-blog"
   ingress_rules         = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks   = ["0.0.0.0/0"]
   egress_rules          = ["all-all"]
