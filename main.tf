db7b03da23c1eb992651266fbb0cc1e21b0703f7
JymmiJamez-MurderBot <James.W.Welch3@gmail.com>
2025-06-16 14:12:22 -0400

    fix CIDR

--- main.tf
+++ main.tf
@@ -20,10 +20,10 @@ module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = var.environment.name
-  cidr = ["${var.environment.network_prefix}.101.0/24", "(${var.environment.network_prefix}).102.0/24", "(${var.environment.network_prefix}).103.0/24"]
+  cidr = "10.0.100.0/22
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "(${var.environment.network_prefix}).102.0/24", "(${var.environment.network_prefix}).103.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.100.0/24", "(${var.environment.network_prefix}).101.0/24", "(${var.environment.network_prefix}).102.0/24"]
 
   tags = {
     Terraform = "true"
