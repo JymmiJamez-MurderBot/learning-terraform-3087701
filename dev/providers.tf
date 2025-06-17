e4e2845d2ed56fff9c7a825aba2f30d627642a5b
JymmiJamez-MurderBot <James.W.Welch3@gmail.com>
2025-06-17 13:19:34 -0400

    update providers

--- dev/providers.tf
+++ dev/providers.tf
@@ -1,10 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
+      version = "6.0.0-beta3"
     }
   }
 }
+
 
 provider "aws" {
   region  = "us-west-2"
