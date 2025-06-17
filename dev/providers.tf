4a3c73bc8b037714c6f5b5d72058ba0a31462d43
JymmiJamez-MurderBot <James.W.Welch3@gmail.com>
2025-06-17 13:39:49 -0400

    fix providers again

--- dev/providers.tf
+++ dev/providers.tf
@@ -1,13 +1,18 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = ">= 5.79.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
-
+# Configure the AWS Provider
 provider "aws" {
-  region  = "us-west-2"
+  region = "us-west-2"
+}
+
+# Create a VPC
+resource "aws_vpc" "dev_vpc" {
+  cidr_block = "10.100.0.0/22"
 }
